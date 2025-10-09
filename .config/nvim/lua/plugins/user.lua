a5c46c0532a1e7d1e7263ceeebdf2cfafe496b80
SBC-Tiendvd <tiendvd@bsscommere.com>
2025-10-09 17:40:05 +0700

    change cursor without smear

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -9,6 +9,9 @@ return {
     "sphamba/smear-cursor.nvim",
 
     opts = {
+      stiffness = 0.5,
+      trailing_stiffness = 0.49,
+      never_draw_over_target = false,
       -- Smear cursor when switching buffers or windows.
       smear_between_buffers = true,
 
