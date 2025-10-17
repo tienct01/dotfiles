2885a80c6802f2bd64af07b7c1829a761b8a0f83
SBC-Tiendvd <tiendvd@bsscommere.com>
2025-10-17 17:07:56 +0700

    bring catppuccin back

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,10 @@
-if true then return {} end
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      flavour = "frappe" -- latte, frappe, macchiato, mocha
     })
     vim.cmd.colorscheme("catppuccin")
   end,
