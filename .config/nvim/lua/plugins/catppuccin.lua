d2e4229730a5ee69cfd7420a41862a7e5bc97807
SBC-Tiendvd <tiendvd@bsscommere.com>
2025-10-17 17:18:27 +0700

    change theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      flavour = "frappe" -- latte, frappe, macchiato, mocha
+      flavour = "mocha" -- latte, frappe, macchiato, mocha
     })
     vim.cmd.colorscheme("catppuccin")
   end,
