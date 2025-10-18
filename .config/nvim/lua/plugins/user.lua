c8582bcbd9a18ae9224d4d40701a40a20e3bee9f
TienDVD <doanvanduytien@gmail.com>
2025-10-18 17:58:01 +0700

    remove smear because it conflict with noice

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -28,32 +28,6 @@ return {
   },
   -- Cursor animation
   {
-    "sphamba/smear-cursor.nvim",
-
-    opts = {
-      stiffness = 0.5,
-      trailing_stiffness = 0.49,
-      never_draw_over_target = false,
-      -- Smear cursor when switching buffers or windows.
-      smear_between_buffers = true,
-
-      -- Smear cursor when moving within line or to neighbor lines.
-      -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
-      smear_between_neighbor_lines = true,
-
-      -- Draw the smear in buffer space instead of screen space when scrolling
-      scroll_buffer_space = true,
-
-      -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
-      -- Smears will blend better on all backgrounds.
-      legacy_computing_symbols_support = false,
-
-      -- Smear cursor in insert mode.
-      -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
-      smear_insert_mode = false,
-    },
-  },
-  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
