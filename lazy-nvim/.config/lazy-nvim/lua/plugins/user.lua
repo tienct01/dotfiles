876370264af58d9d94424812dd6f210f02104fe7
Duy Tien <doanvanduytien@gmail.com>
2025-12-09 16:34:32 +0700

    mrege config to one file

---
+++ lazy-nvim/.config/lazy-nvim/lua/plugins/user.lua
@@ -0,0 +1,32 @@
+return {
+  -- Snacks.nvim
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        ignored = true,
+        hidden = true,
+      },
+    },
+  },
+
+  -- Tmux navigator
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+}
