4d872165b10651b255ef0180692b33383359a9d9
Duy Tien <doanvanduytien@gmail.com>
2025-12-16 17:51:23 +0700

    feat: use vscode diff

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.vscode then return {} end
-return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim",         -- required
-    "sindrets/diffview.nvim",        -- optional - Diff integration
-  },
-}
