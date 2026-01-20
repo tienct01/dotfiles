47ec61e4f0c90dcec65b10d5ca90d61f7d3e166b
Duy Tien <doanvanduytien@gmail.com>
2026-01-20 14:24:12 +0700

    add my zed config

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -3,11 +3,10 @@
 -- fit in the normal config locations above can go here
 
 -- Liquid lsp config
-
 vim.lsp.enable("shopify_theme_ls")
 
+-- Graphql lsp config
 vim.lsp.enable("graphql")
-
 vim.lsp.config("graphql", {
   cmd = { "graphql-lsp", "server", "-m", "stream" },
 })
