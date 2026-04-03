5f9170c498f40f9a4e3d8a768d03b9debfa2e52d
Duy Tien <doanvanduytien@gmail.com>
2026-04-03 09:51:35 +0700

    update

--- nvim/.config/nvim/lua/polish.lua
+++ nvim/.config/nvim/lua/polish.lua
@@ -1,13 +1,5 @@
+if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
-
--- -- Liquid lsp config
--- vim.lsp.enable("shopify_theme_ls")
---
--- -- Graphql lsp config
--- vim.lsp.enable("graphql")
--- vim.lsp.config("graphql", {
---   cmd = { "graphql-lsp", "server", "-m", "stream" },
--- })
-
